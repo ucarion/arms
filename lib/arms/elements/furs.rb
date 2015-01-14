192a99bcdc1d8dcf47d360c40c97543f1aa76d93
Ulysse Carion <ulysse@ulysse.io>
2015-01-14 13:25:23 -0800

    Add ermine.

--- lib/arms/elements/furs.rb
+++ lib/arms/elements/furs.rb
@@ -5,5 +5,11 @@ module Arms
         Nokogiri::XML(load_asset('FleurDeLis')).root
       end
     end
+
+    class Ermine < Fur
+      def to_svg
+        Nokogiri::XML(load_asset('Ermine')).root
+      end
+    end
   end
 end
