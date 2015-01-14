ba25a69c4d7b6697d59472bb54ba721bbbd0cf07
Ulysse Carion <ulysse@ulysse.io>
2015-01-14 10:47:00 -0800

    Move path data into their own files.

---
+++ lib/arms/elements/ordinaries.rb
@@ -0,0 +1,87 @@
+module Arms
+  module Element
+    class Fess < Ordinary
+      def initialize(color)
+        super(color)
+        self.path = load_path
+      end
+    end
+
+    class Fess10 < Ordinary
+      def initialize(color)
+        super(color)
+        self.path = load_path
+      end
+    end
+
+    class Pale < Ordinary
+      def initialize(color)
+        super(color)
+        self.path = load_path
+      end
+    end
+
+    class Pale5 < Ordinary
+      def initialize(color)
+        super(color)
+        self.path = load_path
+      end
+    end
+
+    class Pale6 < Ordinary
+      def initialize(color)
+        super(color)
+        self.path = load_path
+      end
+    end
+
+    class Chief < Ordinary
+      def initialize(color)
+        super(color)
+        self.path = load_path
+      end
+    end
+
+    class Cross < Ordinary
+      def initialize(color)
+        super(color)
+        self.path = load_path
+      end
+    end
+
+    class Chevron < Ordinary
+      def initialize(color)
+        super(color)
+        self.path = load_path
+      end
+    end
+
+    class DoubleChevron < Ordinary
+      def initialize(color)
+        super(color)
+        self.path = load_path
+      end
+    end
+
+    class Bande < Ordinary
+      def initialize(color)
+        super(color)
+        self.path = load_path
+      end
+    end
+
+    class Barre < Ordinary
+      def initialize(color)
+        super(color)
+        self.path = load_path
+      end
+    end
+
+    class Saltire < Ordinary
+      def initialize(color)
+        super(color)
+        self.path = load_path
+      end
+    end
+  end
+end
