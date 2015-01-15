3ab500459e9d7cc4a52669f0aa9ed1106d394a9a
Ulysse Carion <ulysse@ulysse.io>
2015-01-14 16:12:22 -0800

    Add CrossedKeys.

--- lib/arms/elements/charges.rb
+++ lib/arms/elements/charges.rb
@@ -6,5 +6,20 @@ module Arms
         self.path = load_path
       end
     end
+
+    class CrossedKeys < Charge
+      def initialize(key_a_color, key_b_color)
+        @key_a_color = key_a_color
+        @key_b_color = key_b_color
+      end
+
+      def to_svg
+        xml_data = load_asset('CrossedKeys').
+          gsub('{{COLOR_1}}', @key_a_color).
+          gsub('{{COLOR_2}}', @key_b_color)
+
+        Nokogiri::XML(xml_data).root
+      end
+    end
   end
 end
